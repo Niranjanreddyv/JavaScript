5b27d644d6b647c23ae1f79e882c95303ea9d748
Niranjanreddyv <niranjanreddyvakareddy999@gmail.com>
2024-08-15 23:06:21 +0530

    upload files

---
+++ BasicJS/function3.js
@@ -0,0 +1,6 @@
+function multiply(a, b){
+    // a and b are expected inputs
+    console.log(a * b);
+}
+
+multiply(10,20); // 10 and 20 both actual values
---
+++ BasicJS/functions1.js
@@ -0,0 +1,13 @@
+function greeting(){
+    console.log("my logic line1");
+    console.log("my logic line2");
+    console.log("my logic line3....");
+
+}
+
+greeting();
+greeting();
+greeting();
+
+
+console.log(Math.sqrt(100));
