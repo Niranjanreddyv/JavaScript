62f973d420f1e1569f5587ed96be481cbad99f46
Niranjanreddyv <niranjanreddyvakareddy999@gmail.com>
2025-05-21 16:23:22 +0530

    some change1

--- AdvanceJS/OOPS/ObjectDestructuring.js
+++ AdvanceJS/OOPS/ObjectDestructuring.js
@@ -7,7 +7,7 @@ console.log(name);
 console.log(price);
 console.log(rating);
 
-// spraed oprator
+// spread oprator
 
 const Product1 = {oderId:"123456", oderIteam:"xyz", ...Product};
 console.log(Product1); 
